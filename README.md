0c7180fe023e3b20008f55a67a8c73e19f70dcec
ult-zhiwei <zhiwei.zheng@ultiledger.io>
2018-10-18 15:13:17 +0800

    init xrp xlm hd-wallat

---
+++ README.md
@@ -0,0 +1,40 @@
+# hd-wallet
+## Usage
+
+```js
+var HDWallet = require('../src/hd-wallet');
+var coins = require('../src/coins');
+const assert = require('assert');
+
+
+//生成助记词 生成master钱包
+let nm = HDWallet.generateMnemonic();
+console.log(nm);
+let nm2 = 'phone payment tomorrow arrange enable same harsh bacon end initial innocent wet category sea focus brother opinion fever thing rocket venture shy vivid month';
+let wallet = HDWallet.fromMnemonic(nm2);
+
+//生成瑞波账户
+let xrpkeypair = {
+    address:'rJCnMKwAQVfsHGRVQf9LowNJUMNWxftM8K',
+    secret:'shR2Co9jt1Ym3kcZeqWQXRfA6aChA'}
+
+let xrpAccount0 = wallet.getAccount(coins.XRP,0);
+assert.deepEqual(xrpAccount0, xrpkeypair);
+assert.deepEqual(xrpAccount0,HDWallet.getAccountFromSecret(coins.XRP, xrpkeypair.secret));
+
+//生成恒星账户
+let xlmkeypair = { secret: 'SB6BUWP6S5AAAZ4NUFSA463ABXV4GK3UWGNP3JDH757CBGD2KWKM5UM4',
+address: 'GBKKQLDNFLCJHW4GM4YN5H6X2LRCEYBFIPRR3M5URCDJEJM5DFWDRZET' }
+
+let xlmAccount0 = wallet.getAccount(coins.XLM,0);
+assert.deepEqual(xlmAccount0, xlmkeypair);
+assert.deepEqual(xlmAccount0,HDWallet.getAccountFromSecret(coins.XLM, xlmkeypair.secret));
+
+//生成以太坊账户 TODO
+let ethkeypair = { secret: 'SB6BUWP6S5AAAZ4NUFSA463ABXV4GK3UWGNP3JDH757CBGD2KWKM5UM4',
+address: 'GBKKQLDNFLCJHW4GM4YN5H6X2LRCEYBFIPRR3M5URCDJEJM5DFWDRZET' }
+
+let ethAccount0 = wallet.getAccount(coins.XLM,0);
+assert.deepEqual(ethkeypair, xlmkeypair);
+assert.deepEqual(ethkeypair,HDWallet.getAccountFromSecret(coins.ETH, ethkeypair.secret));
+
---
+++ src/coins.js
@@ -0,0 +1,54 @@
+var stellar_Keypair = require('stellar-base').Keypair;
+var ripple_keypairs = require('ripple-keypairs');
+let coins = {
+    'XRP': {
+        name: 'ripple',
+        displayName:'Ripple',
+        ticker:'XRP',
+        derive_path:"m/44'/144'/index'",
+        getKeypair: function(key) {
+            let options = {'entropy':key};
+            const secret = ripple_keypairs.generateSeed(options);
+            const keypair = ripple_keypairs.deriveKeypair(secret);
+            const address = ripple_keypairs.deriveAddress(keypair.publicKey);
+            return { secret, address };
+          },
+        getKeypairFromSecret:function(secret) {
+            const keypair = ripple_keypairs.deriveKeypair(secret);
+            const address = ripple_keypairs.deriveAddress(keypair.publicKey);
+            return { secret, address };
+        }
+     },
+     'XLM': {
+        name: 'stellar',
+        displayName:'Stellar',
+        ticker:'XLM',
+        derive_path:"m/44'/144'/index'",
+        getKeypair: function(key) {
+            const keypair =  stellar_Keypair.fromRawEd25519Seed(key);
+            const address = keypair.publicKey();
+            const secret = keypair.secret();
+            return { secret, address };
+          },
+        getKeypairFromSecret:function(secret) {
+            const keypair = stellar_Keypair.fromSecret(secret);
+            const address = keypair.publicKey();
+            return { secret, address };
+        }
+     },
+     'ETH': {
+        name: 'ethereum',
+        displayName:'Ethereum',
+        ticker:'ETH',
+        derive_path:"m/44'/60'/0'/index'",
+        getKeypair: function(key) {
+            
+            return { secret, address };
+          },
+        getKeypairBySecret:function(secret) {
+
+        }
+     },
+}
+
+module.exports = coins;
